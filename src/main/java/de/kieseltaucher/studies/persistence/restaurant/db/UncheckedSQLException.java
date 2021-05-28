9c068d9a0c429c15d7c5abe3a19b0ccdba6407aa
Bernhard <1688588+berndsed@users.noreply.github.com>
2021-05-28 09:06:14 +0200

    Preserve next sql exception as suppressed exception

--- src/main/java/de/kieseltaucher/studies/persistence/restaurant/db/UncheckedSQLException.java
+++ src/main/java/de/kieseltaucher/studies/persistence/restaurant/db/UncheckedSQLException.java
@@ -1,11 +1,33 @@
 package de.kieseltaucher.studies.persistence.restaurant.db;
 
+import static java.util.function.Predicate.not;
+
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.function.Predicate;
 
 class UncheckedSQLException extends RuntimeException {
 
+    private static Predicate<Throwable> isSuppressed(final Throwable[] suppressed) {
+        return throwable -> Arrays.stream(suppressed)
+            .anyMatch(s -> s == throwable);
+    }
+
     UncheckedSQLException(SQLException cause) {
         super(cause);
+        addChainedExceptions(cause);
+    }
+
+    private void addChainedExceptions(SQLException cause) {
+        final Iterator<Throwable> chained = cause.iterator();
+        final Predicate<Throwable> notAlreadySuppressed = not(isSuppressed(getSuppressed()));
+        while(chained.hasNext()) {
+            final Throwable next = chained.next();
+            if (notAlreadySuppressed.test(next)) {
+                addSuppressed(next);
+            }
+        }
     }
 
 }
---
+++ src/test/java/de/kieseltaucher/studies/persistence/restaurant/db/UncheckedSQLExceptionTest.java
@@ -0,0 +1,47 @@
+package de.kieseltaucher.studies.persistence.restaurant.db;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(Persistence.class)
+class UncheckedSQLExceptionTest {
+
+    @Test
+    void stacktraceContainsChainedSQLExceptions() {
+        final SQLException cause = new SQLException("Root cause");
+        final SQLException chainedException = new SQLException("Another trouble-maker");
+        cause.setNextException(chainedException);
+
+        assertThat(stacktraceOf(new UncheckedSQLException(cause)), containsString(chainedException.getMessage()));
+    }
+
+    @Test
+    void batchExceptions(Connection con) throws SQLException {
+        final Statement stm = con.createStatement();
+        stm.addBatch("insert into UNKNOWNTABLE1 values(1)");
+        stm.addBatch("insert into UNKNOWNTABLE2 values(2)");
+
+        final SQLException sqlException = assertThrows(SQLException.class, stm::executeBatch);
+        final String stackTrace = stacktraceOf(new UncheckedSQLException(sqlException));
+        assertThat(stackTrace, containsString("UNKNOWNTABLE1"));
+        assertThat(stackTrace, containsString("UNKNOWNTABLE2"));
+
+    }
+
+    private String stacktraceOf(Exception e) {
+        final StringWriter writer = new StringWriter();
+        e.printStackTrace(new PrintWriter(writer));
+        return writer.toString();
+    }
+
+}
